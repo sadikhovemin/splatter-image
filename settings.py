6f88fa7700537c41e57c09cdc99c9a0fab55a172
sadikhovemin <eemin.sadykhov@gmail.com>
2024-06-16 19:51:21 +0200

    Preprocess the 3D front data json

---
+++ settings.py
@@ -0,0 +1,4 @@
+GT_TRAIN_SET = "/storage/group/dataset_mirrors/01_incoming/3DFront/3D-FUTURE-scene/GT/train_set.json"
+GT_TEST_SET = "/storage/group/dataset_mirrors/01_incoming/3DFront/3D-FUTURE-scene/GT/test_set.json"
+MODEL_PATH = "/path/to/models"
+LOG_PATH = "/path/to/logs"
